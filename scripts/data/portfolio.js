66f80e5523b3e05f7ca1a08e714be5f4ba748a1a
Tyler Stark <theirondeveloper@gmail.com>
2016-03-27 19:02:28 -0700

    Adding some images for the portfolio

--- scripts/data/portfolio.js
+++ scripts/data/portfolio.js
@@ -101,7 +101,7 @@ module.exports = [
 	}, {
 		name: 'Contact Book on SendMoney pages',
 		description: 'I integrated the contact book found on the send-money pages',
-		image: '',
+		image: './images/portfolio/paypal_contacts.png',
 		link: 'https://www.paypal.com/myaccount/transfer/send',
 		company: 'PayPal',
 		technology: ['Node.js', 'Backbone.js'],
@@ -117,7 +117,7 @@ module.exports = [
 	}, {
 		name: 'SendMoney 3rd-Party Integration',
 		description: 'I created the overall architecture for the sendmoney experience to integrate with 3rd-party experiences including Outlook and PayPal.me',
-		image: '',
+		image: './images/portfolio/paypal_ppme.png',
 		link: 'https://www.paypal.com/myaccount/transfer/',
 		company: 'PayPal',
 		technology: ['JavaScript', 'Node.js', 'CSS'],
@@ -125,7 +125,7 @@ module.exports = [
 	}, {
 		name: 'Pokemon Catch Rate Calculator',
 		description: 'An Ember.js webapp that calculates the odds of catching a Pokemon',
-		image: '',
+		image: './images/portfolio/pokemon_catch_rate.png',
 		link: 'http://theirondeveloper.com/pokemon-catch-rate/',
 		company: 'Personal',
 		technology: ['JavaScript', 'Ember'],
@@ -133,7 +133,7 @@ module.exports = [
 	}, {
 		name: 'Websockets of Life',
 		description: 'A game of life server side demo, powered by jenova and websockets.',
-		image: '',
+		image: './images/portfolio/websockets_life.png',
 		link: 'http://websocket-of-life.herokuapp.com/',
 		company: 'Personal',
 		technology: ['JavaScript', 'Node.js'],
@@ -141,7 +141,7 @@ module.exports = [
 	}, {
 		name: 'Jenova: Game of Life Module',
 		description: 'A game of life module that is intended to work both in a server side and client side environment.',
-		image: '',
+		image: './images/portfolio/websockets_life.png',
 		link: 'https://www.npmjs.com/package/jenova',
 		company: 'Personal',
 		technology: ['JavaScript', 'Node.js'],
@@ -149,7 +149,7 @@ module.exports = [
 	}, {
 		name: 'Outlook.com\'s PayPal App',
 		description: 'I served as the tech-lead for integrating PayPal\'s send money experience onto Outlook.com ',
-		image: '',
+		image: './images/portfolio/paypal_outlook.png',
 		link: 'https://store.office.com/paypal-for-outlook-WA104379615.aspx',
 		company: 'PayPal',
 		technology: ['JavaScript', 'Node.js'],
